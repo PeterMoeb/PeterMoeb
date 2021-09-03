ced182b7e08937b525d850b800c2f3d1bcd12a3c
PeterMoeb <90031347+PeterMoeb@users.noreply.github.com>
2021-09-03 13:31:58 +0200

    Update blink00A.ino

--- blink00A.ino
+++ blink00A.ino
@@ -1,7 +1,13 @@
 // Primitiv-Test Blinken LED
+
 /* So geht ein Kommentar über mehrere Zeilen!
 Ja, so geht das!
 Es sind 3 Zeilen. */
+
+// Hier ist es aber nur eine Zeile
+
+/* Aber hier sind es zwei.
+   Ja. */
 
 
 int ledPin = 13;
